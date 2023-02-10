a31ed9e6fc08927be173b60823f47f50b23ec62c
devrabaioli <devrabaioli@gmail.com>
2023-02-10 12:13:37 +0000

    GET by PageRequest

--- src/main/java/dev/rabaioli/lafapp/resources/CategoryResource.java
+++ src/main/java/dev/rabaioli/lafapp/resources/CategoryResource.java
@@ -1,11 +1,13 @@
 package dev.rabaioli.lafapp.resources;
 
 import java.net.URI;
+
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import dev.rabaioli.lafapp.domain.Category;
 import dev.rabaioli.lafapp.dto.CategoryDTO;
 import dev.rabaioli.lafapp.services.CategoryService;
@@ -30,6 +32,17 @@ public class CategoryResource {
 		Category obj = service.findbyId(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoryDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="name") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Category> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoryDTO> listDto = list.map(obj -> new CategoryDTO(obj));  
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
--- src/main/java/dev/rabaioli/lafapp/services/CategoryService.java
+++ src/main/java/dev/rabaioli/lafapp/services/CategoryService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import dev.rabaioli.lafapp.domain.Category;
@@ -21,6 +24,11 @@ public class CategoryService {
 	public Category findbyId(Integer id) {
 		Optional<Category> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found, invalid id: " + id + " Class: " + Category.class.getName()));
+	}
+	
+	public Page<Category> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 	public Category insert(Category obj) {
@@ -48,5 +56,6 @@ public class CategoryService {
 	public List<Category> findAll(){
 		return repo.findAll();
 	}
+
 
 }
